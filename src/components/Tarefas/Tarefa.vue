cb30265009516ac4c96059fb95e865fcbf666517
brunomdr <brunomdr46@gmail.com>
2022-07-15 15:14:12 -0300

    Ações e mutações com Vuex

--- src/components/Tarefas/Tarefa.vue
+++ src/components/Tarefas/Tarefa.vue
@@ -1,6 +1,8 @@
 <template>
   <q-item
-    @click="tarefa.completada = !tarefa.completada"
+    @click="
+      atualizarTarefa({ id: id, updates: { completada: !tarefa.completada } })
+    "
     :class="!tarefa.completada ? 'bg-orange-1' : 'bg-green-1'"
     clickable
     v-ripple
@@ -24,18 +26,46 @@
           <q-item-label class="row justify-end" caption>{{
             tarefa.dataPrazo
           }}</q-item-label>
-          <q-item-label caption>
+          <q-item-label class="row justify-end" caption>
             <small>{{ tarefa.horarioPrazo }}</small>
           </q-item-label>
         </div>
       </div>
     </q-item-section>
+
+    <q-item-section side>
+      <q-btn
+        @click.stop="avisoDeletar(id)"
+        flat
+        round
+        dense
+        color="negative"
+        icon="delete"
+      />
+    </q-item-section>
   </q-item>
 </template>
 
 <script>
+import { mapActions } from "vuex";
+
 export default {
   props: ["tarefa", "id"],
+  methods: {
+    ...mapActions("tarefas", ["atualizarTarefa", "deletarTarefa"]),
+    avisoDeletar(id) {
+      this.$q
+        .dialog({
+          title: "Confirmação",
+          message: "Você quer realmente deletar essa tarefa?",
+          cancel: true,
+          persistent: true,
+        })
+        .onOk(() => {
+          this.deletarTarefa(id);
+        });
+    },
+  },
 };
 </script>
 
--- src/store/store-tarefas.js
+++ src/store/store-tarefas.js
@@ -1,3 +1,5 @@
+import Vue from "vue";
+
 const state = {
   tarefas: {
     ID1: {
@@ -19,34 +21,26 @@ const state = {
       horarioPrazo: "18:00",
     },
   },
-  // tarefas: [
-  //   {
-  //     id: 1,
-  //     nome: "Comprar bananas",
-  //     completada: false,
-  //     dataPrazo: "17/07/2022",
-  //     horarioPrazo: "18:30",
-  //   },
-  //   {
-  //     id: 2,
-  //     nome: "Enviar e-mail para o coordenador",
-  //     completada: false,
-  //     dataPrazo: "29/07/2022",
-  //     horarioPrazo: "12:00",
-  //   },
-  //   {
-  //     id: 3,
-  //     nome: "Marcar consulta na dentista",
-  //     completada: false,
-  //     dataPrazo: "15/11/2022",
-  //     horarioPrazo: "18:00",
-  //   },
-  // ],
 };
 
-const mutations = {};
+const mutations = {
+  atualizarTarefa(state, payload) {
+    console.log(payload);
+    Object.assign(state.tarefas[payload.id], payload.updates);
+  },
+  deletarTarefa(state, id) {
+    delete state.tarefas[id]
+  },
+};
 
-const actions = {};
+const actions = {
+  atualizarTarefa({ commit }, payload) {
+    commit("atualizarTarefa", payload);
+  },
+  deletarTarefa({ commit }, id) {
+    commit("deletarTarefa", id);
+  },
+};
 
 const getters = {
   tarefas: (state) => {
